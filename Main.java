f241ad84b80687d2b29907d13879ef54059cfe50
tranquanghuu322 <mizeks322@gmail.com>
2018-10-18 20:16:08 +0700

    BAlo_ThamLam
    Bài toán ba lô về tham lam

---
+++ Main.java
@@ -0,0 +1,12 @@
+package BTBalo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BaiToanBaLo btbalo = new BaiToanBaLo();
+		btbalo.thucThiBaLo();
+	}
+
+}
